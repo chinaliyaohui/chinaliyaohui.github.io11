17a4394e262fb6aa9c48ee8f80d647daf1cadb1f
chinaliyaohui <52875044+chinaliyaohui@users.noreply.github.com>
2019-10-22 21:41:44 +0800

    Delete index.php

--- index.php
+++
@@ -1,3 +0,0 @@
-	<?php  
-		header("location:http://pm-shopbase.oss-cn-qingdao.aliyuncs.com/certificate/2019/09/20/a5db321fad4a3653dbf5dca28223b169.pdf?spm=a2142.3063657.0.0.7ba45ae5kCVAtm&file=a5db321fad4a3653dbf5dca28223b169.pdf") 
-	?>
