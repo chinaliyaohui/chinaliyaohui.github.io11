57bb79692820c3b9eea300af6e9de346a269a4cd
chinaliyaohui <52875044+chinaliyaohui@users.noreply.github.com>
2019-10-22 12:59:03 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,3 @@
+	<?php  
+		header("location:http://pm-shopbase.oss-cn-qingdao.aliyuncs.com/certificate/2019/09/20/a5db321fad4a3653dbf5dca28223b169.pdf?spm=a2142.3063657.0.0.7ba45ae5kCVAtm&file=a5db321fad4a3653dbf5dca28223b169.pdf") 
+	?>
